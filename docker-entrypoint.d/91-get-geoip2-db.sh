013740c2e15398eebd723bc3560ca2069f71e21a
Saiful Alam <saiful.alam@accezatech.com>
2025-05-01 13:37:22 -0400

    added openssl35 support, add patch for native quic support in nginx 1.28

--- docker-entrypoint.d/91-get-geoip2-db.sh
+++ docker-entrypoint.d/91-get-geoip2-db.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+mkdir -p /etc/nginx/geoip
 curl -sSfL https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-Country.mmdb -o /etc/nginx/geoip/GeoLite2-Country.mmdb
 curl -sSfL https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb -o /etc/nginx/geoip/GeoLite2-City.mmdb
 curl -sSfL https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-ASN.mmdb -o /etc/nginx/geoip/GeoLite2-ASN.mmdb
--- docker-entrypoint.d/92-get-dhparam.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-curl -fSL https://ssl-config.mozilla.org/ffdhe2048.txt > /etc/nginx/certs/dhparam.pem
